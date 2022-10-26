fa948a19da7c3afd9188911a37997d05acc35fca
harismajeed <hm79405@gmail.com>
2022-10-26 20:40:59 +0500

    full api example

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,23 +1,53 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateEventDto } from './create.events.dto';
+import { Events } from './event.entity';
+import { UpdateEventDto } from './update.event.dto';
 
-@Controller('/events') 
+@Controller('/events')
 export class EventsController {
-    @Get()  
-    findAll(){}
-    @Get(':id')
-    findOne(@Param('id') id:string) {
-        return id;
-    }
-    @Post()
-     create(@Body() input:object) {
-        return input;
-     }
-    @Patch(':id') 
-     update(@Param('id') id:string, @Body() input:object) {
-        return {id,input};
-     }
-    @Delete(':id')
-     remove(@Param('id') id:string) {
-        return id;
-     }
+  private events: Events[] = [];
+  @Get()
+  findAll() {
+    return this.events;
+  }
+  @Get(':id')
+  findOne(@Param('id') id: any) {
+    const event = this.events.find((event) => event.id === parseInt(id));
+    return event;
+  }
+  @Post()
+  create(@Body() input: CreateEventDto) {
+    const event = {
+      ...input,
+      when: new Date(input.when),
+      id: this.events.length + 1,
+    };
+    this.events.push(event);
+    return event;
+  }
+  @Patch(':id')
+  update(@Param('id') id: any, @Body() input: UpdateEventDto) {
+    const index = this.events.findIndex((event) => event.id === parseInt(id));
+    this.events[index] = {
+      ...this.events[index],
+      ...input,
+      when: input.when ? new Date(input.when) : this.events[index].when,
+    };
+    return this.events[index];
+  }
+  @Delete(':id')
+  @HttpCode(204)
+  remove(@Param('id') id: any) {
+    this.events = this.events.filter((event) => event.id === parseInt(id));
+
+  }
 }
---
+++ src/update.event.dto.ts
@@ -0,0 +1,6 @@
+import { PartialType } from "@nestjs/mapped-types";
+import { CreateEventDto } from "./create.events.dto";
+
+export class UpdateEventDto extends PartialType(CreateEventDto){
+
+}
